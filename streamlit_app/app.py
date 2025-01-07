b77f5539e205009844fe45da4caa9a3c0e90ff9b
Филипп <Fyuburlakov@edu.hse.ru>
2025-01-07 14:32:33 +0300

    Added cache for metadata stats, changed app workflow

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,11 +1,10 @@
-from typing import List, Tuple, Dict, Union, Any
+from typing import List
 from pathlib import Path
 from collections import Counter
 import json
 
 import asyncio
 import aiohttp
-import requests
 import yadisk
 from translate import Translator
 import pandas as pd
@@ -16,11 +15,12 @@ import spacy
 t = Translator(to_lang='en', from_lang='ru')
 nlp = spacy.load("ru_core_news_sm")
 TMP_PATH = './tmp/'
+Path(TMP_PATH).mkdir(parents=True, exist_ok=True)
 
 
 async def get_vids(query: str, top: int, token: str, disk_emb_path: str) -> List[int]:
     '''Обращение к бэку'''
-    link = "http://fastapi:8000"  # вот это я хз, правильно ли
+    link = "http://fastapi:8000"
     async with aiohttp.ClientSession(trust_env=True) as session:
         async with session.post(f'{link}/get_vids',
                                 json={'text': [query],
@@ -33,33 +33,51 @@ async def get_vids(query: str, top: int, token: str, disk_emb_path: str) -> List
     return idxs
 
 
-async def clear_path(path: Path):
-    '''Красивый путь'''
-    if path.exists():
-        for node in path.iterdir():
-            node.unlink()
-    else:
-        path.mkdir(parents=True)
+def get_client():
+    if st.session_state['token']:
+        st.session_state['yadisk_client'] = yadisk.Client(token=st.session_state['token'])
 
 
-def get_data() -> Tuple[str, str, Any]:
+def set_path():
+    if st.session_state['yadisk_path']:
+        st.session_state['yadisk_path'] += '/' \
+            if not st.session_state['yadisk_path'].endswith('/') \
+            else ''
+
+
+def get_data():
     '''Скачивание данных с Яндекс Диска'''
-    token = st.text_input(
+    st.text_input(
         "Enter your YaDisk Key:",
-        type="password"
+        type="password",
+        key='token',
+        on_change=get_client,
     )
 
-    path = st.text_input("Enter your YaDisk path:")
+    st.text_input(
+        "Enter your YaDisk path:",
+        key='yadisk_path',
+        on_change=set_path
+    )
 
-    meta_file = st.file_uploader(
+    st.file_uploader(
         "Drop your JSON metadata:",
-        type='json'
+        type='json',
+        on_change=count_stats,
+        key='meta_file'
     )
-    return token, path, meta_file
 
 
-def count_stats(meta: List[Dict]) -> Tuple[Counter, Counter, Counter, List[float], List[int]]:
+def count_stats():
     '''EDA'''
+    if st.session_state['meta_file'] is None:
+        Path(TMP_PATH + 'stats.json').unlink()
+        st.session_state.pop('meta_list')
+        return None
+
+    meta = json.loads(st.session_state['meta_file'].getvalue())
+    st.session_state['meta_list'] = meta
+
     lemmas = Counter()
     bigrams = Counter()
     trigrams = Counter()
@@ -84,87 +102,71 @@ def count_stats(meta: List[Dict]) -> Tuple[Counter, Counter, Counter, List[float
                         trigrams.update(
                             [doc[i - 2].lemma_ + ' ' + doc[i - 1].lemma_ + ' ' + doc[i].lemma_]
                         )
-    return lemmas, bigrams, trigrams, durations, word_counts
+    json.dump(
+        {
+            'lemmas': [[val, lemmas[val]] for val in sorted(lemmas, key=lemmas.get, reverse=True)[:50]],
+            'bigrams': [[val, bigrams[val]] for val in sorted(bigrams, key=bigrams.get, reverse=True)[:50]],
+            'trigrams': [[val, trigrams[val]] for val in sorted(trigrams, key=trigrams.get, reverse=True)[:50]],
+            'duration': durations,
+            'word count': word_counts
+        },
+        Path(TMP_PATH + 'stats.json').open('w', encoding='utf-8'),
+        indent=2,
+        ensure_ascii=False
+    )
 
 
-async def draw_graphs(meta: List[Dict]):
-    lemmas, bigrams, trigrams, durations, word_counts = count_stats(meta)
+async def draw_graphs():
+    stats = json.load(Path(TMP_PATH + 'stats.json').open('r', encoding='utf-8'))
+
     left, right = st.columns(2)
 
     with left:
         option = st.selectbox(
             "A phrase feature to display distribution for:",
-            ("Word count", "Duration")
+            ("word count", "duration"),
+            format_func=lambda x: x.title(),
+            index=0
         )
 
-        if option == 'Word count':
-            st.plotly_chart(
-                violin(
-                    {"num. of words": word_counts},
-                    box=True
-                ),
-                use_container_width=True
-            )
-        elif option == 'Duration':
-            st.plotly_chart(
-                violin(
-                    {"duration, s": durations},
-                    box=True
-                ),
-                use_container_width=True
-            )
+        st.plotly_chart(
+            violin(
+                {option: stats[option]},
+                box=True
+            ),
+            use_container_width=True
+        )
 
     with right:
         num = st.selectbox(
             "Most popular text features to show:",
-            ("Lemmas", "Bigrams", "Trigrams")
+            ("lemmas", "bigrams", "trigrams"),
+            format_func=lambda x: x.title(),
+            index=0
         )
 
-        if num == "Lemmas":
-            st.bar_chart(
-                pd.DataFrame([
-                    {
-                        'lemmas': pair[0],
-                        'values': pair[1]
-                    }
-                    for pair in lemmas.most_common(50)
-                ]),
-                x="lemmas",
-                y="values"
-            )
-        elif num == "Bigrams":
-            st.bar_chart(
-                pd.DataFrame([
-                    {
-                        'bigrams': pair[0],
-                        'values': pair[1]
-                    }
-                    for pair in bigrams.most_common(50)
-                ]),
-                x="bigrams",
-                y="values"
-            )
-        elif num == "Trigrams":
-            st.bar_chart(
-                pd.DataFrame([
-                    {
-                        'trigrams': pair[0],
-                        'values': pair[1]
-                    }
-                    for pair in trigrams.most_common(50)
-                ]),
-                x="trigrams",
-                y="values"
-            )
+        st.bar_chart(
+            pd.DataFrame([
+                {
+                    num: pair[0],
+                    'values': pair[1]
+                }
+                for pair in stats[num]
+            ]),
+            x=num,
+            y="values"
+        )
 
 
-async def process_query(query: str, top_n: int, meta: List[Dict],
-                        path: str, client: yadisk.Client, token: str):
+async def process_query(query: str, top_n: int):
     query = t.translate(query)
-    vid_idxs = await get_vids(query, top_n, token, path)
+    meta = st.session_state['meta_list']
+    vid_idxs = await get_vids(query, top_n, st.session_state['token'], st.session_state['yadisk_path'])
     for idx in vid_idxs:
         if not Path(TMP_PATH + meta[idx]['video']).exists():
-            client.download(path + meta[idx]['video'], TMP_PATH + meta[idx]['video'])
+            st.session_state['yadisk_client'].download(
+                st.session_state['yadisk_path'] + meta[idx]['video'], TMP_PATH + meta[idx]['video']
+            )
         st.video(
             TMP_PATH + meta[idx]['video'],
             start_time=meta[idx]['start'],
@@ -175,33 +177,32 @@ async def process_query(query: str, top_n: int, meta: List[Dict],
 
 
 async def main():
-    token, path, meta_file = get_data()
+    get_data()
 
-    if token and path and meta_file:
-        meta = json.loads(meta_file.getvalue())
-        await draw_graphs(meta)
+    if st.session_state['meta_file']:
+        await draw_graphs()
 
-        client = yadisk.Client(token=token)
-        path += '/' if not path.endswith('/') else ''
+    left, right = st.columns([5, 1])
+    with left:
+        query = st.text_area(
+            "Enter your query:",
+            placeholder="Интервью с сурдолимпийским чемпионом..."
+        )
 
-        left, right = st.columns([5, 1])
-        with left:
-            query = st.text_area(
-                "Enter your query:",
-                placeholder="Интервью с сурдолимпийским чемпионом..."
-            )
+    with right:
+        top_n = st.number_input(
+            "Enter maximum vids:",
+            min_value=1,
+            max_value=10,
+            value="min",
+            step=1
+        )
 
-        with right:
-            top_n = st.number_input(
-                "Enter maximum vids:",
-                min_value=1,
-                max_value=10,
-                value="min",
-                step=1
-            )
-
-        if query:
-            await process_query(query, top_n, meta, path, client, token)
+    if (query and
+            'meta_list' in st.session_state and
+            st.session_state['yadisk_path'] and
+            'yadisk_client' in st.session_state):
+        await process_query(query, top_n)
 
 
 if __name__ == '__main__':
